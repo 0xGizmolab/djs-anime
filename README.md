5210ffa3fa210e8e94f622b489534cc136bf7804
Abbas Ali <51985250+gizmo-dev@users.noreply.github.com>
2021-07-17 22:57:35 +0530

    Djs v13 update

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# djs-anime
- A Discord Package for Anime Commands
+# Note
+DISCORD V12 - npm i djs-anime
+DISCORD V13 - npm i djs-anime@dev
+
+# About
+A discord.js Anime Package with With Many Anime Related Commands and Features!
+Join our [Support Server](https://discord.gg/jDP2FbvCdk) for help
+# Installation
+
+```npm i djs-anime```
+
+# Example usage
+
+```
+USE ACCORDING TO YOUR COMMAND HANDLER
+```
+
+**ANIME GIFs**
+=== 
+
+```js
+ const { Anime } = require('djs-anime')
+    const gif = new Anime({
+    message: message,
+    embedFooter: "Testing", //The Footer of the embed
+    embedTitle: "Anime Gifs", //The title of the embed
+    embedColor: "RANDOM", //The color of the embed! (Use Hex codes or use the color name)
+    })
+    gif.hug()
+
+```
+### AVAILABLE CATAGORIES:
+Category | usage |
+|---|---|
+| Cuddle| gif.cuddle() |
+| Kiss | gif.kiss() | 
+| Hug | gif.hug() | 
+| Bully | gif.kiss() | 
+| Poke | gif.poke() | 
+| Kick | gif.kick() |
+| Slap | gif.slap() |
+| Happy | gif.happy() |
+| Kill | gif.kill() |
+| Wink | gif.wink() |
+| Dance | gif.dance() |
+| Cringe | gif.cringe() |
+| Bite | gif.bite() | 
+| Handhold | gif.handhold() |   
+| Wave| gif.wave() |
+| highfive | gif.highfive() |       
+| Smile | gif.smile() |   
+| Blush | gif.blush() | 
+| Yeet | gif.yeet() |
+| Bonk | gif.bonk() |  
+| Smug | gif.smug() | 
+| Pat | gif.pat() |
+| Lick | gif.lick() |
+| Cry | gif.cry() |   
+
+
+
+
+# Credits
+API USED: https://waifu.pics/
+
+# Help
+
+Join our [Support Server](https://discord.gg/jDP2FbvCdk) for help
---
+++ data/images.js
@@ -0,0 +1,588 @@
+class Anime {
+    constructor(options) {
+
+        if (!options.embedFooter) throw new TypeError('Missing argument embedFooter')
+        if (typeof options.embedFooter !== 'string') throw new TypeError('embedFooter must be a string')
+
+        if (!options.embedTitle) throw new TypeError('Missing argument embedTitle')
+        if (typeof options.embedTitle !== 'string') throw new TypeError('embedTitle must be a string')
+
+        if (!options.embedColor) throw new TypeError('Missing argument embedColor')
+        if (typeof options.embedColor !== 'string') throw new TypeError('embedColor must be a string')
+        if (!options.message) throw new TypeError('Missing argument: message')
+
+        this.message = options.message;
+        this.embedFooter = options.embedFooter
+        this.embedTitle = options.embedTitle
+        this.embedColor = options.embedColor
+    }
+   //Random Cuddle
+    async cuddle() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/cuddle';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+//Random hug
+    async hug() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/hug';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+    //Random Kiss
+    async kiss() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/kiss';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+//Random Smile
+    async smile() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/smile';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+    //Random lick
+    async lick() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/lick';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+    //Random wave
+    async wave() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/wave';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+    //Random handhold
+    async handhold() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/handhold';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+    //Random wink
+    async wink() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/wink';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+    //Random poke
+    async poke() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/poke';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Dance
+    async dance() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/dance';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Cringe
+    async cringe() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/cringe';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Kill
+    async kill() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/kill';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Slap
+    async slap() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/slap';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random bite
+    async bite() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/bite';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Highfive
+    async highfive() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/highfive';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Blush
+    async blush() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/blush';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random pat
+    async pat() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/pat';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random smug
+    async smug() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/smug';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random bonk
+    async bonk() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/bonk';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Cry
+    async cry() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/cry';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Bully
+    async bully() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/bully';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Yeet
+    async yeet() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/yeet';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Happy
+    async happy() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/happy';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+    //Random Kick
+    async kick() {
+        const Discord = require('discord.js');
+        const axios = require('axios');
+
+        const url = 'https://api.waifu.pics/sfw/kick';
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return this.message.channel.send(`An error occured!`)
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(this.embedTitle)
+            .setColor(this.embedColor)
+            .setImage(data.url)
+            .setFooter(this.embedFooter)
+
+        await this.message.channel.send({ embeds: [embed] })
+    }
+
+}
+module.exports = Anime;
